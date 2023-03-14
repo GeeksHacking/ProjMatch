3e124c004169ed107bfb5445d1ad2843caf9bae7
Ethan Chew <chew_ming_hong_ethan@s2019.ssts.edu.sg>
2023-03-14 11:34:02 +0800

    Started the Login Screen

---
+++ src/components/Landing/HeaderBar.js
@@ -0,0 +1,19 @@
+export default function HeaderBar() {
+    return (
+        <div className="flex flex-row relative w-full p-2 z-50">
+            <span className="font-bold text-2xl text-logo-blue">ProjMatch</span>
+            <div className="ml-auto space-x-3">
+                <button className="bg-light-blue p-2 rounded-lg text-white font-bold">
+                    <Link href={{pathname: "/pages/Landing/UserEntry", query: "login"}}>
+                        Log In
+                    </Link>
+                </button>
+                <button className="bg-light-blue p-2 rounded-lg text-white font-bold">
+                    <Link href={{pathname: "/pages/Landing/UserEntry", query: "signup"}}>
+                        Sign Up
+                    </Link>
+                </button>
+            </div>
+        </div>
+    )
+}
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,16 +1,14 @@
-const Landing = () => {
+import Link from "next/link"
+import { useState } from "react"
 
+// Components
+import HeaderBar from "@/components/Landing/HeaderBar"
+
+const Landing = () => {
 
     return (
         <div className='w-full h-full'>
-            {/* Header Bar */}
-            <div className="flex flex-row relative w-full p-2">
-                <span className="font-bold text-2xl text-logo-blue">ProjMatch</span>
-                <div className="ml-auto space-x-3">
-                    <button className="bg-light-blue p-2 rounded-lg text-white font-bold">Log In</button>
-                    <button className="bg-light-blue p-2 rounded-lg text-white font-bold">Sign Up</button>
-                </div>
-            </div>
+            <HeaderBar />
         </div>
     )
 }
