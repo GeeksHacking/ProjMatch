889c6eef1159074940fe3d08b6a88f32b528f469
Jerick Seng <64419693+jer123se12@users.noreply.github.com>
2023-03-21 16:26:39 +0800

    sidebar updates

--- src/components/SideNav/MaxSideNav.js
+++ src/components/SideNav/MaxSideNav.js
@@ -1,5 +1,6 @@
 // Components Import
 import Logo from "./Logo"
+import styles from './SideNav.module.css'
 
 const MaxSideNav = () => {
 
@@ -14,9 +15,12 @@ const MaxSideNav = () => {
     
 
     return (
-        <div className="max-nav absolute z-0 top-0 left-0 h-full w-fit">
-            <div className={`bg-light-blue h-full w-fit flex flex-col items-left pt-3 pb-3`}>
-                <Logo fullSideNav={true}/>
+        <div className={`fixed z-0 top-0 left-0 h-full w-fit`}>
+            <div className={`${styles.SideNav} bg-light-blue h-full w-fit flex flex-col items-left pt-3 pb-3`}>
+                <a className={`flex items-center space-x-2 text-logo-blue pl-3 pr-3`}>
+                   <img src="/logo.svg" alt="logo" className='w-12 h-12 flex-shrink-0'></img>
+                   <span className={`${styles.SideNavTxt} font-bold text-xl`}> ProjMatch </span>
+                </a>
                 <div className={`mt-10 space-y-4`}>
                     <ul className={`space-y-4`}>
                         {navOptions.map((option) => (
@@ -27,14 +31,14 @@ const MaxSideNav = () => {
                 <div className={`mt-auto space-y-6 p-3`}>
                     <a className={`flex items-center space-x-4 text-light-blue bg-logo-blue w-full h-fit rounded-lg`}>
                         <img src="/NavBarIcons/IconsCreate.svg" alt="logo" className='w-8 h-8 flex-shrink-0 ml-[0.6875rem] my-2'></img>
-                        <span className='font-bold text-xl flex items-center pb-0.5'> Create </span>
+                        <span className={`${styles.SideNavTxt} font-bold text-xl flex items-center pb-0.5`}> Create </span>
                     </a>
 
                     <a className={`flex items-center flex-row space-x-2`}>
                         <img src="/NavBarIcons/IconsProfile.jpg" alt="logo" className='w-14 h-14 flex-shrink-0 rounded-full border-2 border-logo-blue'></img>
                         <div className="flex items-start flex-col">
-                            <span className='font-bold text-lg text-logo-blue translate-y-0.5'> MrJohnDoe </span>
-                            <span className='font-bold text-lg text-start -translate-y-0.5'> John Doe </span>
+                            <span className={`${styles.SideNavTxt} font-bold text-lg text-logo-blue translate-y-0.5`}> MrJohnDoe </span>
+                            <span className={`${styles.SideNavTxt} font-bold text-lg text-start -translate-y-0.5`}> John Doe </span>
                         </div>
                         
                     </a>
@@ -50,7 +54,7 @@ const SideNavOptn = ({option}) => {
         <div className={`sidenav-btn pt-2 pb-2 p-5 pt-3 pb-3`}>
             <a className='flex items-center space-x-4 text-logo-blue'>
                 <img src={option.IconPath} alt="logo" className='w-8 h-8 flex-shrink-0'></img>
-                <span className='font-bold text-xl'>{option.Page}</span>
+                <span className={`font-bold text-xl ${styles.SideNavTxt}`}>{option.Page}</span>
             </a>
         </div>
     )
