b20894ca62db6224151add7fc3733475664737fe
Ethan-Chew <chew_ming_hong_ethan@s2019.ssts.edu.sg>
2023-05-02 22:52:26 +0800

    Finished up Picker

--- src/components/ImagePicker/ImagePicker.js
+++ src/components/ImagePicker/ImagePicker.js
@@ -1,46 +1,60 @@
 import styles from "./ImagePicker.module.css"
+import { useEffect, useState } from "react"
 
 const ImagePicker = ({ images }) => {
+
+    const [ imgs, setImgs ] = useState([])
+
+    useEffect(() => {
+        setImgs(images)
+    }, [])
+
+    const handleSubmission = (e) => {
+        const rawFiles = [...e.target.files]
+        let fileURLs = []
+
+        for (let i = 0; i < rawFiles.length; i++) {
+            const fileURL = URL.createObjectURL(rawFiles[i])
+            if (fileURL !== undefined) {
+                fileURLs.push(fileURL)
+            } else {
+                console.error("Could not convert file to URL")
+            }
+        }
+
+        let temp = imgs.concat(fileURLs)
+        setImgs(temp)
+    }
+
+    const deleteImage = (img) => {
+        let temp = imgs
+        const index = temp.indexOf(img)
+
+        temp.splice(index, 1)
+        setImgs(temp)
+    }
+
     return (
         <div className="flex flex-row gap-4 pt-4">
             {
-                images.map(img => (
-                    <ImageObj img={img} />
+                imgs.map(img => (
+                    <div className={`${styles.addedImage}`} id={img}>
+                        <img src={img}></img>
+                        <div className={`${styles.imageHoverContainer}`}>
+                            <button onClick={() => deleteImage(img)}>Delete</button>
+                        </div>
+                    </div>
                 ))
             }
-            <AddImageIcon />
-        </div>
-    )
-}
 
-const ImageObj = ({ img }) => {
-
-    const onClickImage = () => {
-
-    }
-
-    return (
-        <div className={`${styles.addedImage}`}>
-            <img src={img}></img>
-            <div className={`${styles.imageHoverContainer}`}>
-                <button>Delete</button>
+            <div className={`bg-slate-200 ${styles.addParent}`}>
+                <form onChange={handleSubmission}>
+                    <div className={`bg-slate-300 ${styles.addChild}`}>
+                        <p className="text-2xl font-bold">+</p>
+                    </div>
+                    <input multiple type="file" name="imageInput" className={`${styles.inputImage}`}></input>
+                </form>
             </div>
-        </div>
-    )
-}
-
-const AddImageIcon = () => {
-
-    const doOnClick = () => {
-        console.log("Cicked")
-    }
-
-    return (
-        <div className={`bg-slate-200 ${styles.addParent}`}>
-            <div className={`bg-slate-300 ${styles.addChild}`} onClick={doOnClick}>
-                <p className="text-2xl font-bold">+</p>
-            </div>
-            {/* <input type="file" name="imageInput" className={`${styles.inputImage}`}></input> */}
         </div>
     )
 }
