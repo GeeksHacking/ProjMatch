65a6813410badf36d38e22cf8c8f5a94e3430a50
Irman Zaf <irman_zafyree_bin_irmanizam@s2019.ssts.edu.sg>
2023-06-19 21:36:14 +0800

    fix: pathing to tags.json on search

--- src/pages/Main/SearchPage.js
+++ src/pages/Main/SearchPage.js
@@ -1,146 +1,167 @@
 import SideNav from "@/components/SideNav/SideNav";
 import PMApi from "@/components/PMApi/PMApi";
-import { withPageAuthRequired, getAccessToken } from "@auth0/nextjs-auth0"
-import Link from "next/link"
-import {useUser} from "@auth0/nextjs-auth0/client"
-import axios from "axios"
-import { use, useCallback, useEffect, useState } from "react"
+import { withPageAuthRequired, getAccessToken } from "@auth0/nextjs-auth0";
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import axios from "axios";
+import { use, useCallback, useEffect, useState } from "react";
 import { Listbox } from "@headlessui/react";
-import approvedTags from "../tags.json";
-let api=0
+import approvedTags from ".../tags.json";
+let api = 0;
 export default function SearchPage() {
-    const { user, error, isLoading } = useUser()
-    const [ posts, setPosts ] = useState([])
-    const [ search, setSearch ] = useState("")
-    const [ tagsFilter, setTagsFilter ] = useState([])
-    const [ filteredPosts, setFilteredPosts ] = useState([])
-    useEffect(()=>{
-        const authToken = localStorage.getItem("authorisation_token")
-        if (!(authToken===undefined)){
-            api=new PMApi(authToken)
-        }
-    },[])
-    
-    const getPostsWithSearch = useCallback(async (search) => {
-        api.getPosts({"search":search}).then(function (res){
-            if (res!=-1){
-                setPosts(res.posts)
-            }
-        })
-    }, [])
+	const { user, error, isLoading } = useUser();
+	const [posts, setPosts] = useState([]);
+	const [search, setSearch] = useState("");
+	const [tagsFilter, setTagsFilter] = useState([]);
+	const [filteredPosts, setFilteredPosts] = useState([]);
+	useEffect(() => {
+		const authToken = localStorage.getItem("authorisation_token");
+		if (!(authToken === undefined)) {
+			api = new PMApi(authToken);
+		}
+	}, []);
 
-    useEffect(() => {
-        if (search === "") {
-            
-            getPostsWithSearch(search)
-        }
-    }, [search])
+	const getPostsWithSearch = useCallback(async (search) => {
+		api.getPosts({ search: search }).then(function (res) {
+			if (res != -1) {
+				setPosts(res.posts);
+			}
+		});
+	}, []);
 
-    const handleSearch = (e) => {
-        e.preventDefault()
-        setSearch(e.target.value)
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        
-        getPostsWithSearch(search)
-    }
+	useEffect(() => {
+		if (search === "") {
+			getPostsWithSearch(search);
+		}
+	}, [search]);
 
-    useEffect(() => {
+	const handleSearch = (e) => {
+		e.preventDefault();
+		setSearch(e.target.value);
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-        if (tagsFilter.length !== 0) {
-            setFilteredPosts(posts.filter((post) => {
-                for (let i = 0; i < tagsFilter.length; i++) {
-                    if (post.tags.indexOf(tagsFilter[i]) !== -1) {
-                        return true
-                    }
-                }
-                return false
-            }))
-        }
+		getPostsWithSearch(search);
+	};
 
-        if (tagsFilter.length === 0) {
-            setFilteredPosts(posts)
-        }
+	useEffect(() => {
+		if (tagsFilter.length !== 0) {
+			setFilteredPosts(
+				posts.filter((post) => {
+					for (let i = 0; i < tagsFilter.length; i++) {
+						if (post.tags.indexOf(tagsFilter[i]) !== -1) {
+							return true;
+						}
+					}
+					return false;
+				})
+			);
+		}
 
-    }, [tagsFilter])
+		if (tagsFilter.length === 0) {
+			setFilteredPosts(posts);
+		}
+	}, [tagsFilter]);
 
-    useEffect(() => {
-        if (posts.length !== 0) {
-            setFilteredPosts(posts)
-        }
-    }, [posts])
+	useEffect(() => {
+		if (posts.length !== 0) {
+			setFilteredPosts(posts);
+		}
+	}, [posts]);
 
-    if (isLoading) return <div>Loading...</div>
-    if (error) return <div>{error.message}</div>
-    if (!user) return <div>Not logged in</div>;
-    return (
-        <div className='absolute flex w-full h-full flex-col justify-start items-center'>
-            <SideNav/>
-            <div className='absolute flex w-[70%] h-full flex-col justify-start items-center my-10'>
-                <h1 className="text-6xl font-bold text-black">Search</h1>
-                <div className="relative flex flex-row justify-start items-center w-full h-16 rounded-lg border-2 border-[#D3D3D3] px-2 mt-10">
-                    <img src="/Search.svg" alt="Search" className="h-[60%] ml-2 mr-4"/>
-                    <input type="text" name="search" placeholder="Search for any project!" className="border-1 border-white w-full" onChange={handleSearch}/>
-                </div>
-                <div className="flex flex-row w-full justify-end items-center mt-5">
-                    <Listbox multiple value={tagsFilter} onChange={setTagsFilter}>
-                        <Listbox.Button className="bg-logo-lblue rounded-lg px-5 py-2 text-lg font-bold text-white mr-1">
-                            Filter
-                        </Listbox.Button>
-                        <div className="relative">
-                            <Listbox.Options className="absolute top-6 left-[-300px] w-[300px] h-[500px] bg-white z-[1000] border-2 border-logo-blue rounded-lg overflow-y-scroll">
-                                {approvedTags.map((tag, index) => (
-                                    (tagsFilter.indexOf(tag) === -1) ? 
-                                        <Listbox.Option key={index} value={tag} className="rounded p-2 h-8 bg-white flex flex-row items-center">
-                                            <p className="font-light text-base">{tag}</p>
-                                        </Listbox.Option>
-                                        : <Listbox.Option key={index} value={tag} className="p-2 h-8 bg-logo-blue flex flex-row items-center">
-                                            <p className="text-white font-bold text-base">{tag}</p>
-                                        </Listbox.Option>
-                                ))}
-                            </Listbox.Options>
-                        </div>
-                    </Listbox>
-                    <button className="bg-logo-blue rounded-lg px-5 py-2 text-lg font-bold text-white ml-1" onClick={handleSubmit}>
-                        Search
-                    </button>
-                </div>
-                <div className="w-full h-fit relative grid grid-cols-3 gap-6 my-14">
-                    {filteredPosts.map((post) => (
-                        <Project post={post} key={post._id}/>
-                    ))}
-                </div>
-            </div>
-        </div>
-    )
+	if (isLoading) return <div>Loading...</div>;
+	if (error) return <div>{error.message}</div>;
+	if (!user) return <div>Not logged in</div>;
+	return (
+		<div className="absolute flex h-full w-full flex-col items-center justify-start">
+			<SideNav />
+			<div className="absolute my-10 flex h-full w-[70%] flex-col items-center justify-start">
+				<h1 className="text-6xl font-bold text-black">Search</h1>
+				<div className="relative mt-10 flex h-16 w-full flex-row items-center justify-start rounded-lg border-2 border-[#D3D3D3] px-2">
+					<img src="/Search.svg" alt="Search" className="ml-2 mr-4 h-[60%]" />
+					<input
+						type="text"
+						name="search"
+						placeholder="Search for any project!"
+						className="border-1 w-full border-white"
+						onChange={handleSearch}
+					/>
+				</div>
+				<div className="mt-5 flex w-full flex-row items-center justify-end">
+					<Listbox multiple value={tagsFilter} onChange={setTagsFilter}>
+						<Listbox.Button className="mr-1 rounded-lg bg-logo-lblue px-5 py-2 text-lg font-bold text-white">
+							Filter
+						</Listbox.Button>
+						<div className="relative">
+							<Listbox.Options className="absolute left-[-300px] top-6 z-[1000] h-[500px] w-[300px] overflow-y-scroll rounded-lg border-2 border-logo-blue bg-white">
+								{approvedTags.map((tag, index) =>
+									tagsFilter.indexOf(tag) === -1 ? (
+										<Listbox.Option
+											key={index}
+											value={tag}
+											className="flex h-8 flex-row items-center rounded bg-white p-2"
+										>
+											<p className="text-base font-light">{tag}</p>
+										</Listbox.Option>
+									) : (
+										<Listbox.Option
+											key={index}
+											value={tag}
+											className="flex h-8 flex-row items-center bg-logo-blue p-2"
+										>
+											<p className="text-base font-bold text-white">{tag}</p>
+										</Listbox.Option>
+									)
+								)}
+							</Listbox.Options>
+						</div>
+					</Listbox>
+					<button
+						className="ml-1 rounded-lg bg-logo-blue px-5 py-2 text-lg font-bold text-white"
+						onClick={handleSubmit}
+					>
+						Search
+					</button>
+				</div>
+				<div className="relative my-14 grid h-fit w-full grid-cols-3 gap-6">
+					{filteredPosts.map((post) => (
+						<Project post={post} key={post._id} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export function Project({post}) {
-    let tagString = ''
-    if (post.tags.length !== 0) {
-        tagString += post.tags[0]
-        if (post.tags.length > 1) {
-            for (let i = 1; i < (post.tags.length > 3 ? 3 : post.tags.length); i++) {
-                tagString += ', ' + post.tags[i]
-            }
-        }
-    }
+export function Project({ post }) {
+	let tagString = "";
+	if (post.tags.length !== 0) {
+		tagString += post.tags[0];
+		if (post.tags.length > 1) {
+			for (let i = 1; i < (post.tags.length > 3 ? 3 : post.tags.length); i++) {
+				tagString += ", " + post.tags[i];
+			}
+		}
+	}
 
-    let imageLink = "http://placekitten.com/800/600"
-    if (post.images !== null) {
-        if (post.images.length !== 0) {
-            imageLink = post.images[0]
-        }
-    }
+	let imageLink = "http://placekitten.com/800/600";
+	if (post.images !== null) {
+		if (post.images.length !== 0) {
+			imageLink = post.images[0];
+		}
+	}
 
-    return (
-        <a className="z-10 relative w-full aspect-[4/3] flex flex-col justify-center items-center rounded-lg" href={"/ProjectPage/?id="+post._id}>
-            <div className="z-10 absolute bg-white/[0.5] w-full h-1/4 bottom-0 rounded-b-lg px-4 flex flex-col justify-center items-start">
-                <h3 className="text-xl font-semibold">{post.projectName}</h3>
-                <p className="text-lg font-light">{tagString}</p>
-            </div>
-            <img src={imageLink} className="z-0 w-full h-full rounded-lg"></img>
-        </a>
-    )
+	return (
+		<a
+			className="relative z-10 flex aspect-[4/3] w-full flex-col items-center justify-center rounded-lg"
+			href={"/ProjectPage/?id=" + post._id}
+		>
+			<div className="absolute bottom-0 z-10 flex h-1/4 w-full flex-col items-start justify-center rounded-b-lg bg-white/[0.5] px-4">
+				<h3 className="text-xl font-semibold">{post.projectName}</h3>
+				<p className="text-lg font-light">{tagString}</p>
+			</div>
+			<img src={imageLink} className="z-0 h-full w-full rounded-lg"></img>
+		</a>
+	);
 }
